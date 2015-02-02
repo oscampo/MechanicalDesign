248769baf38804be5418fc3b56eeab78af3099e9
oscampo <oscampo@hotmail.com>
2015-02-02 04:33:52 +0000

    Some comments to fstools

--- functions/fstools.py
+++ functions/fstools.py
@@ -1,20 +1,21 @@
 #coding: utf-8
+#utilidades para funciones de singularidad
 from numpy import *
 from sympy import solve, Symbol, init_printing
 
-def fsin(x,a,n): #defnicion de una función de singularidad
+def fsin(x,a,n): #defnición de una función de singularidad
 	return pow((x-a),n)*(x>a)*(n>=0)
 
-def ifsin(m): #definicion de la integral de fsin
+def ifsin(m): #definición de la integral de fsin
 	return vstack([m[0,:]/pow(m[2,:]+1,(m[2,:]+1)>0),m[1,:],m[2,:]+1])
 
-def mat2fsin(m,x): #definicion de una funcion que convierte un modelo de viga de expresion matricial a algebraica
+def mat2fsin(m,x): #definicón de una funcion que convierte un modelo de viga de expresion matricial a algebraica
 	fil,col=m.shape
 	if isinstance(x,float):
 		f=x*0
 		for i in range(col):
 			f=f+m[0,i]*fsin(x,m[1,i],m[2,i])
-	else:
+	else: #si x es una variable simbólica, mat2fsin devuelve una representación algebráica de la función de singularidad
 		f=Symbol(str(m[0,0])+str(' \\langle ')+str(x)+str('-')+str(m[1,0])+str(' \\rangle^{')+str(m[2,0])+str('}'))
 		for i in range(col-1):
 			f=Symbol(str(m[0,i+1])+str(' \\langle ')+str(x)+str('-')+str(m[1,i+1])+str(' \\rangle^{')+str(m[2,i+1])+str('}'))+f
